ba5e9647d78e8ca96a496afb4874e9578e478f44
otgit <otgit@yahoo.com>
2019-08-24 21:32:48 +0900

    Initial commit

---
+++ src/main/java/us/flower/dayary/DayaryApplication.java
@@ -0,0 +1,13 @@
+package us.flower.dayary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayaryApplication.class, args);
+    }
+
+}
