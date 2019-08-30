53311233ca2e0720584afeaf470d4b0866eb78f0
jongui <jongui@pointpark.com>
2019-08-30 10:12:56 +0900

    Member -> People 변경

--- src/main/java/us/flower/dayary/repository/PersonRepository.java
+++ src/main/java/us/flower/dayary/repository/PersonRepository.java
@@ -1,7 +1,8 @@
 package us.flower.dayary.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import us.flower.dayary.domain.People;
 
-public interface PersonRepository extends JpaRepository<Person, Long>{
+public interface PersonRepository extends JpaRepository<People, Long>{
 
 }
