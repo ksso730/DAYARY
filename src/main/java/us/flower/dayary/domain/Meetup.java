45ea31f2c8fe604e3d2f04dbc30a90b8600938fb
jongui <jongui@pointpark.com>
2019-08-30 10:34:19 +0900

    Meetup vo 추가

---
+++ src/main/java/us/flower/dayary/domain/Meetup.java
@@ -0,0 +1,19 @@
+package us.flower.dayary.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * 오프라인모임
+ */
+@Entity
+@Table(name="MEETUP")
+@Data
+public class Meetup {
+
+    @Id
+    @GeneratedValue
+    @Column(name="NO")
+    private long no;
+}
