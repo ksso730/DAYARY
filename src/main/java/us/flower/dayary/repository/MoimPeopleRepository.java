28a1de4ebf8f9f29ee851f8718fae8581851984d
otgit <otgit@yahoo.com>
2019-09-02 22:30:10 +0900

    오타제거

--- src/main/java/us/flower/dayary/repository/MoimPeopleRepository.java
+++ src/main/java/us/flower/dayary/repository/MoimPeopleRepository.java
@@ -1,10 +1,7 @@
 package us.flower.dayary.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import us.flower.dayary.domain.Moim;
 import us.flower.dayary.domain.MoimPeople;
-import us.flower.dayary.domain.MoimPeopleId;
 
 public interface MoimPeopleRepository extends JpaRepository<MoimPeople, Long>{
 
