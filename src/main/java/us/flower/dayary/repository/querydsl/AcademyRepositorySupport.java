ab19c13fe64fd79348410d3789187e47b90c7eff
jongui <jongui@pointpark.com>
2019-09-06 09:17:37 +0900

    쿼리디에스엘

--- src/main/java/us/flower/dayary/repository/querydsl/AcademyRepositorySupport.java
+++ src/main/java/us/flower/dayary/repository/querydsl/AcademyRepositorySupport.java
@@ -1,29 +1,31 @@
 package us.flower.dayary.repository.querydsl;
 
 
-import java.util.List;
-
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import us.flower.dayary.domain.Academy;
+import us.flower.dayary.domain.QAcademy;
+
+import java.util.List;
 
 @Repository
 public class AcademyRepositorySupport extends QuerydslRepositorySupport {
     private final JPAQueryFactory queryFactory;
-    
+
+    QAcademy academy = QAcademy.academy;
 
     public AcademyRepositorySupport(JPAQueryFactory queryFactory) {
         super(Academy.class);
         this.queryFactory = queryFactory;
     }
-//    public List<Academy> findByName(String name) {
-//        return queryFactory
-//                .selectFrom(academy)
-//                .where(academy.name.eq(name))
-//                .fetch();
-//    }
+
+
+    public List<Academy> findByName(String name) {
+        return queryFactory
+                .selectFrom(academy)
+                .where(academy.name.eq(name))
+                .fetch();
+    }
 
 } 
--- src/test/java/us/flower/dayary/BasicTest.java
+++ src/test/java/us/flower/dayary/BasicTest.java
@@ -1,48 +1,48 @@
-//package us.flower.dayary;
-//
-//import static org.hamcrest.CoreMatchers.is;
-//import static org.junit.Assert.assertThat;
-//
-//import java.util.List;
-//
-//import org.junit.After;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import us.flower.dayary.domain.Academy;
-//import us.flower.dayary.repository.AcademyRepository;
-//import us.flower.dayary.repository.querydsl.AcademyRepositorySupport;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class BasicTest {
-//
-//    @Autowired
-//    private AcademyRepository academyRepository;
-//
-//    @Autowired
-//    AcademyRepositorySupport academyRepositorySupport;
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        academyRepository.deleteAllInBatch();
-//    }
-//
-//    @Test
-//    public void querydsl_Test() {
-//        //given
-//        String name = "jojoldu";
-//        String address = "jojoldu@gmail.com";
-//        academyRepository.save(new Academy(name, address));
-//
-//        //when
-////        List<Academy> result = academyRepositorySupport.findByName(name);
-//
-//        //then
-//        assertThat(result.size(), is(1));
-//        assertThat(result.get(0).getAddress(), is(address));
-//    }
-//}
+package us.flower.dayary;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import us.flower.dayary.domain.Academy;
+import us.flower.dayary.repository.AcademyRepository;
+import us.flower.dayary.repository.querydsl.AcademyRepositorySupport;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicTest {
+
+    @Autowired
+    private AcademyRepository academyRepository;
+
+    @Autowired
+    AcademyRepositorySupport academyRepositorySupport;
+
+    @After
+    public void tearDown() throws Exception {
+        academyRepository.deleteAllInBatch();
+    }
+
+    @Test
+    public void querydsl_Test() {
+        //given
+        String name = "jojoldu";
+        String address = "jojoldu@gmail.com";
+        academyRepository.save(new Academy(name, address));
+
+        //when
+        List<Academy> result = academyRepositorySupport.findByName(name);
+
+        //then
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0).getAddress(), is(address));
+    }
+}
