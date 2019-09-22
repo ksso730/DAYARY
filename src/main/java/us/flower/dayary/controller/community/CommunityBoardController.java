492feb5eac6986e42c8aeb9f0505ef14933e565e
Minho Lee <alsgkgk77@gmail.com>
2019-09-22 22:25:12 +0900

    자유게시판 글 저장

--- src/main/java/us/flower/dayary/controller/community/CommunityBoardController.java
+++ src/main/java/us/flower/dayary/controller/community/CommunityBoardController.java
@@ -6,17 +6,32 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.HttpSessionRequiredException;
+import org.springframework.web.bind.annotation.*;
 import us.flower.dayary.domain.BoardGroup;
 import us.flower.dayary.domain.CommunityBoard;
+import us.flower.dayary.domain.People;
 import us.flower.dayary.repository.community.CommunityBoardRepository;
+import us.flower.dayary.repository.people.PeopleRepository;
+import us.flower.dayary.service.PeopleService;
+import us.flower.dayary.service.community.CommunityBoardService;
+
+import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
 public class CommunityBoardController {
 
 	@Autowired CommunityBoardRepository communityBoardRepository;
+
+	@Autowired
+	CommunityBoardService communityBoardService;
+
+	@Autowired
+	PeopleRepository peopleRepository;
 	/**
 	 * 커뮤니티/스터디리스트 조회
 	 *
@@ -48,6 +63,27 @@ public class CommunityBoardController {
 	}
 
 	/**
+	 *
+	 * @param board_group_no
+	 * @param session
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/community/communityList/studyWrite/{board_group_no}")
+	public String studyWrite(@PathVariable("board_group_no") long board_group_no, HttpSession session, Model model) {
+
+		// board group
+		model.addAttribute("board_group_no",board_group_no);
+
+		// people name
+		long peopleId = (Long) session.getAttribute("peopleId");
+		People people = peopleRepository.getOne(peopleId);
+		model.addAttribute("name", people.getName());
+
+		return "community/comunitystudyWrite";
+	}
+
+	/**
      * 커뮤니티리스트 글쓰기
      *
      * @param
@@ -55,10 +91,41 @@ public class CommunityBoardController {
      * @throws
      * @author minholee
      */
-	@GetMapping("/community/communityList/studyWrite/{board_group_no}")
-	public String studyWrite(@PathVariable("board_group_no") long board_group_no) {
+	@ResponseBody
+	@PostMapping("/community/communityList/studyWrite/{board_group_no}")
+	public Map<String, Object> studyWrite(@PathVariable("board_group_no") long board_group_no, @RequestBody CommunityBoard communityBoard,
+							 HttpSession session) {
 
-		return "community/comunitystudyWrite";
+		Map<String, Object> returnData = new HashMap<String, Object>();
+
+		if (communityBoard.getTitle().equals(null) || communityBoard.getTitle().equals("")) {
+			returnData.put("code", "0");
+			returnData.put("message", "제목을 입력해주세요");
+			return returnData;
+		}
+
+		if (communityBoard.getMemo().equals(null) || communityBoard.getMemo().equals("")) {
+			returnData.put("code", "0");
+			returnData.put("message", "내용을 입력해주세요");
+			return returnData;
+		}
+
+		//SimpleDateFormat format = new SimpleDateFormat ( "yyyy년 MM월dd일 HH시mm분ss초");
+		//String format_time = format.format (System.currentTimeMillis());
+
+		Long people_no = (Long) session.getAttribute("peopleId");//사용자세션정보 들고오기
+
+		try {
+			communityBoardService.communityWrite(people_no,board_group_no,communityBoard);
+			returnData.put("code", "1");
+			returnData.put("message", "저장되었습니다");
+
+		} catch (Exception e) {
+			returnData.put("code", "E3290");
+			returnData.put("message", "데이터 확인 후 다시 시도해주세요.");
+		}
+
+		return returnData;
 	}
 
 	 /**
--- src/main/java/us/flower/dayary/service/community/CommunityBoardService.java
+++ src/main/java/us/flower/dayary/service/community/CommunityBoardService.java
@@ -14,7 +14,7 @@ public interface CommunityBoardService {
 
 
     List<CommunityBoard> CommunityList();
-	Page<CommunityBoard> CommunityStudyList(BoardGroup boardGroup, Pageable pageable);
+	//Page<CommunityBoard> CommunityStudyList(BoardGroup boardGroup, Pageable pageable);
 
 	void deleteBoardone(long timeLineListNo);
 }
