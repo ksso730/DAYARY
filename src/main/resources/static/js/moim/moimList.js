2e17d56295440ac48c2399653df50a5c1d17bf25
choiseongjun <csj2702@naver.com>
2019-09-29 00:14:32 +0900

    모임리스트 화면 변경

--- src/main/resources/static/js/moim/moimList.js
+++ src/main/resources/static/js/moim/moimList.js
@@ -27,5 +27,54 @@ function initMoimCategoryList(opt){
          $('#categorybox').selectmenu('refresh');
       } 
    });
-   
 }
+
+/**
+ * 
+ * @시/도/구 카테고리 조회 API
+ */
+$(function(){
+	
+	$.ajax({
+		type: "get",
+		url: "http://openapi.nsdi.go.kr/nsdi/eios/service/rest/AdmService/admCodeList.json",
+		data : {authkey : $('#sido_key').val()},
+		async: false,
+		dataType: 'json',
+		success: function(data) {
+			var html = "<option>선택</option>";
+			
+			for(var i=0;i<data.admVOList.admVOList.length;i++){ 
+				html +="<option value='"+data.admVOList.admVOList[i].admCode+"'>"+data.admVOList.admVOList[i].lowestAdmCodeNm+"</option>"
+			}
+			
+            $('#sido_code').html(html);
+			
+		},
+		error: function(xhr, stat, err) {}
+	});
+	
+	
+	$(document).on("change","#sido_code",function(){
+		var thisVal = $(this).val();		
+		
+		$.ajax({
+			type: "get",
+			url: "http://openapi.nsdi.go.kr/nsdi/eios/service/rest/AdmService/admSiList.json",
+			data : {admCode : thisVal, authkey : $('#sigoon_key').val()},
+			async: false,
+			dataType: 'json',
+			success: function(data) {
+				var html = "<option>선택</option>";
+				
+				for(var i=0;i<data.admVOList.admVOList.length;i++){ 
+					html +="<option value='"+data.admVOList.admVOList[i].admCode+"'>"+data.admVOList.admVOList[i].lowestAdmCodeNm+"</option>"
+				}
+				
+	            $('#sigoon_code').html(html);
+				
+			},
+			error: function(xhr, stat, err) {}
+		});
+	});
+})
