010cd05a96035261bfaeba2218d36f7d77bb0ad9
choiseongjun <csj2702@naver.com>
2019-09-28 16:03:12 +0900

    회원정보페이지 회원정보 값 랜더링,접속자 방문수 초기작업

---
+++ src/main/java/us/flower/dayary/repository/visitcheck/VisitCheckRepository.java
@@ -0,0 +1,9 @@
+package us.flower.dayary.repository.visitcheck;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import us.flower.dayary.domain.VisitCheck;
+
+public interface VisitCheckRepository extends JpaRepository<VisitCheck, Long>{
+
+}
