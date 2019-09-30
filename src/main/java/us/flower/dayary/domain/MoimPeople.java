6a312eb9786c6ef88bbae20fae086faba16425b6
choiseongjun <csj2702@naver.com>
2019-09-30 13:28:09 +0900

    moimpeople 시퀀스수정

--- src/main/java/us/flower/dayary/domain/MoimPeople.java
+++ src/main/java/us/flower/dayary/domain/MoimPeople.java
@@ -27,22 +27,14 @@ import us.flower.dayary.domain.common.DateAudit;
  */
 @Entity
 @Table(name = "MOIM_PEOPLE")
-@SequenceGenerator(
-        name="MOIM_PEOPLE_GEN", //시퀀스 제너레이터 이름
-        sequenceName="MOIM_PEOPLE_SEQ", //시퀀스 이름
-        initialValue=1, //시작값
-        allocationSize=1 //메모리를 통해 할당할 범위 사이즈
-        )
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MoimPeople extends DateAudit{
 
 	@Id
-	@GeneratedValue(
-            strategy=GenerationType.SEQUENCE, //사용할 전략을 시퀀스로  선택
-            generator="MOIM_PEOPLE_SEQ" //식별자 생성기를 설정해놓은  GEN으로 설정        
-            )
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "ID")
 	private long id;
 	@ManyToOne
