cf90b5ea92ec6260f56436ac27e8ca3ce226031a
choiseongjun <csj2702@naver.com>
2019-10-01 11:29:39 +0900

    에러 수정

--- src/main/java/us/flower/dayary/controller/moim/board/MoimFreeBoardController.java
+++ src/main/java/us/flower/dayary/controller/moim/board/MoimFreeBoardController.java
@@ -1,8 +1,10 @@
 package us.flower.dayary.controller.moim.board;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Controller
 public class MoimFreeBoardController {
 
 	/**
