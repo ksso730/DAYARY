307a701e1e3163eed6db9aff2f5c3f3ceab40706
choiseongjun <csj2702@naver.com>
2019-10-01 16:52:55 +0900

    회원강퇴 진행중

--- src/main/resources/static/js/moim/moimDetail.js
+++ src/main/resources/static/js/moim/moimDetail.js
@@ -2,7 +2,37 @@ function myPeopleCount() {
   console.log("페이지 로딩 완료");
 
 }
-
+$('#banpeople_btn').off().on('click', function () {//회원 강퇴하기 by choiseongjun 2019-10-01
+	  var myValue = $(this).html();
+	  alert(myValue); 
+	var moimNo = $('#moimNo').attr("data-moimNo");
+	var peopleIdKind = $('#peopleIdKind').text();
+	moimNo*=1;
+	  console.log(moimNo);
+	  console.log(peopleIdKind);
+//	 $.ajax({
+//	    	url : '/moimParticipant/banjoinedPeople/'+moimNo, 
+//	    	type : "DELETE",   
+//	    	processData: false, //데이터를 쿼리 문자열로 변환하는 jQuery 형식 방지
+//	        contentType: false,
+//	        contentType: 'application/json; charset=UTF-8',
+//	    	dataType   : 'json',  
+//	    	data	   : JSON.stringify(moimPeopleNo),
+//	        success:function(data){
+//	        	if(data.code==1){
+//	        		console.log("success callback data");
+//	        		 alert(data.message);
+//	                 location.href='/moimlistView';
+//				}else{
+//					alert(data.message);
+//				}
+//	        },
+//	        error:function(e){
+//
+//	        }
+//	    });
+	
+});
 $('#signup_btn').off().on('click', function () {//스터디 가입하기 by choiseongjun 2019-09-20
 
     var moimNo = $('#moimNo').attr("data-moimNo");
