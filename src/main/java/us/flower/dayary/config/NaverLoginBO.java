0ff4dc654e699836a2cfab11ca02adb7099bd83f
hmyn11 <kynkd11@gmail.com>
2019-10-24 18:08:38 +0900

    c

--- src/main/java/us/flower/dayary/config/NaverLoginBO.java
+++ src/main/java/us/flower/dayary/config/NaverLoginBO.java
@@ -27,9 +27,9 @@ public class NaverLoginBO {
 	private final static String SESSION_STATE = "oauth_state";
 	/* 프로필 조회 API URL */
 	private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
-
+ 
 	/* 네이버 아이디로 인증 URL 생성 Method */
-	public String getAuthorizationUrl(HttpSession session) {
+	public String getAuthorizationUrl(HttpSession session) { 
 		/* 세션 유효성 검증을 위하여 난수를 생성 */
 		String state = generateRandomString();
 		/* 생성한 난수 값을 session에 저장 */
