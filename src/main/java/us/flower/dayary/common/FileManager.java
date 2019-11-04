d85e64a0ccdf089e7c7221f8b42a494fd40d5f68
Minho Lee <alsgkgk77@gmail.com>
2019-11-04 20:38:08 +0900

    커뮤니티 파일 저장

--- src/main/java/us/flower/dayary/common/FileManager.java
+++ src/main/java/us/flower/dayary/common/FileManager.java
@@ -33,53 +33,70 @@ public class FileManager {
     }
 
     /**
-     * 2019-10-23 임시 (minholee)
+     * 파일저장
      * @param file
      * @param uploadPath
      * @return
      */
     public static String fileSave(String uploadPath, MultipartFile file) throws IllegalStateException, IOException {
+
+        // create file object
         File uploadPathDir = new File(uploadPath);
 
+        // make directory if not exist
         if(!uploadPathDir.exists()){
             uploadPathDir.mkdirs();
         }
 
+        // create UUID
         String genId = UUID.randomUUID().toString();
         genId = genId.replace("-","");
 
+        // save filename : UUID + File Extension
+        // save filename : SLEI@LEI@%&WLSL@.png
+        // original filename : image.png
         String originalFileName = file.getOriginalFilename();
         String fileExtension = getExtension(originalFileName);
         String saveFileName = genId + "." + fileExtension;
 
+        // make directory root + (year/month/day)
         String savePath = calcPath(uploadPath);
 
+        // uploadPath : /C:/dayary/community/image
+        // savePath : /2019/11/04
         File target = new File(uploadPath + savePath, saveFileName);
+
+        // copy file bytes to target directory
         FileCopyUtils.copy(file.getBytes(), target);
+
 
         return makeFilePath(uploadPath, savePath, saveFileName);
     }
 
 
     /**
-     * 2019-10-23 임시 (minholee)
+     * 파일 생성날짜의 년/월/일에 해당하는 Directory 생성
      * @param uploadPath
      * @return
      */
     private static String calcPath(String uploadPath) {
+        // get calandar instance
         Calendar cal = Calendar.getInstance();
 
+        // get calc Path (year/month/day)
         String yearPath = File.separator + cal.get(Calendar.YEAR);
         String monthPath = yearPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.MONTH));
         String datePath = monthPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.DATE));
 
+        // make directory
         makeDir(uploadPath, yearPath, monthPath, datePath);
 
+        // return final directory path
         return datePath;
     }
 
     /**
-     * 2019-10-23 임시 (minholee)
+     * 상위 directory부터 없으면 생성
      * @param uploadPath
      * @param paths
      */
@@ -97,7 +114,7 @@ public class FileManager {
     }
 
     /**
-     * 2019-10-23 임시 (minholee)
+     * root 를 제외한 file directory (/2019/11/04/file.jpg)
      * @param uploadPath
      * @param savePath
      * @param saveFileName
