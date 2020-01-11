239de392285882df06b6800b8a7d187fb467a4a1
choiseongjun <csj2702@naver.com>
2020-01-11 17:14:02 +0900

    파일 오류 수정

--- src/main/java/us/flower/dayary/domain/File.java
+++ src/main/java/us/flower/dayary/domain/File.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
 import lombok.Data;
 import us.flower.dayary.domain.common.DateAudit;
 
